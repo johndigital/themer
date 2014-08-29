96478ee8784b286dd3c9151bdbac3ed4d4b2be2f
John <john@funkhaus.us>
2014-08-29 16:19:35 -0700

    Added mobile theme switching

--- devthemer.php
+++ devthemer.php
@@ -10,17 +10,24 @@
 
 ! defined( 'ABSPATH' ) and exit;
 
-add_filter( 'template', 'devt_switch_to_dev' );
-add_filter( 'stylesheet', 'devt_switch_to_dev' ); // only WP smaller 3*
-add_filter( 'option_template', 'devt_switch_to_dev' );
-add_filter( 'option_stylesheet', 'devt_switch_to_dev' );
+add_filter( 'template', 'devt_switch_theme' );
+add_filter( 'stylesheet', 'devt_switch_theme' ); // only WP 3* and below
+add_filter( 'option_template', 'devt_switch_theme' );
+add_filter( 'option_stylesheet', 'devt_switch_theme' );
 
-function devt_switch_to_dev( $template = '' ) {
+function devt_switch_theme( $template = '' ) {
+
+	// Get all themes
+	$themes = wp_get_themes();
 
 	// Check if user is administrator, set template if so
 	if ( current_user_can( 'manage_options' ) ) {
-		$themes = wp_get_themes();
-		$selected = get_option('devt_theme');
+		$selected = get_option('devt_admin_theme');
+		if ( isset($themes[$selected]) ) {
+			$template = $selected;
+		}
+	} elseif ( wp_is_mobile() ) {
+		$selected = get_option('devt_mobile_theme');
 		if ( isset($themes[$selected]) ) {
 			$template = $selected;
 		}
@@ -42,18 +49,32 @@ function devt_switch_to_dev( $template = '' ) {
 			<form action="options.php" method="post" id="devt_settings">
 				<?php settings_fields('devt_settings'); ?>
 				<?php $themes = wp_get_themes(); ?>
+				<?php $current_theme = wp_get_theme(); ?>
 				<table class="form-table">
 					<tbody>
 						<tr valign="top">
-							<th scope="row"><label>Select Dev Theme:</label></th>
+							<th scope="row"><label>Admins users will see:</label></th>
 							<td>
-								<select name="devt_theme" id="devt_theme">
-									<option value="0" <?php if ( ! get_option('devt_theme')) echo 'selected'; ?>>None</option>
+								<select name="devt_admin_theme" id="devt_admin_theme">
+									<option value="0" <?php if ( ! get_option('devt_admin_theme')) echo 'selected'; ?>>Default Theme</option>
 									<?php foreach ( $themes as $slug => $theme ) : ?>
-										<option value="<?php echo $slug; ?>" <?php selected( get_option('devt_theme'), $slug ); ?>><?php echo $slug; ?></option>
+										<option value="<?php echo $slug; ?>" <?php selected( get_option('devt_admin_theme'), $slug ); ?>><?php echo $theme->get( 'Name' ); ?></option>
 									<?php endforeach; ?>
 								</select>
 							</td>
+
+						</tr>
+						<tr valign="top">
+							<th scope="row"><label>Mobile users will see:</label></th>
+							<td>
+								<select name="devt_mobile_theme" id="devt_mobile_theme">
+									<option value="0" <?php if ( ! get_option('devt_mobile_theme')) echo 'selected'; ?>>Default Theme</option>
+									<?php foreach ( $themes as $slug => $theme ) : ?>
+										<option value="<?php echo $slug; ?>" <?php selected( get_option('devt_mobile_theme'), $slug ); ?>><?php echo $theme->get( 'Name' ); ?></option>
+									<?php endforeach; ?>
+								</select>
+							</td>
+							
 						</tr>
 					</tbody>
 				</table>
@@ -68,7 +89,8 @@ function devt_switch_to_dev( $template = '' ) {
 
 	/* Save Takeover Settings */
 	function devt_settings_init(){
-		register_setting('devt_settings', 'devt_theme');
+		register_setting('devt_settings', 'devt_admin_theme');
+		register_setting('devt_settings', 'devt_mobile_theme');
 	}
 	add_action('admin_init', 'devt_settings_init');
 
